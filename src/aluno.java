ba82fcdd445068280187438bf01b04946416bc42
ra21502520 <ra21502520@AN800629077417.dinfor.uniceub>
2017-05-12 22:14:36 -0300

    adicionar aluno

---
+++ src/aluno.java
@@ -0,0 +1,17 @@
+
+public class aluno extends Pessoa {
+
+	void finalizaCadastro (String curso, String disciplina, String turma) {
+		// insere dados no banco
+		
+		
+	}
+	
+	
+	void setAluno (boolean admin, long matricula, String senha, String nome) {
+		//toma mais
+		
+		
+	}
+	
+}
