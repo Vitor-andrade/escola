d7058307e67e85177769ba1240324c9e5fac4ab4
RA21500213 <RA21500213@AN800630077434.dinfor.uniceub>
2017-05-22 22:37:23 -0300

    mudanca curso

---
+++ src/projEscola/Curso.java
@@ -0,0 +1,53 @@
+package projEscola;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+import servicos.ConexaoBanco;
+
+public class Curso {
+	static String host = "127.0.0.1";
+	static int port = 3306;
+	static String dbName = "escola";
+	static String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+	static ConexaoBanco conexao;
+	static Connection con = null;
+	static PreparedStatement stmt = null;
+	
+	public Curso(){
+		this.conexao = ConexaoBanco.getInstance();
+		this.con = conexao.getConexao();
+	}
+	
+	public Vector buscaTurmaCadastro() throws SQLException {
+		
+		stmt = this.con.prepareStatement("select x.curso from escola.materias x");
+		ResultSet result = stmt.executeQuery();
+	    ResultSetMetaData metaData = (ResultSetMetaData) result.getMetaData();
+	 // Cabeçário
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // Data
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (result.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(result.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return data;
+
+	}
+
+}
--- src/projEscola/cadastroTurma.java
+++ src/projEscola/cadastroTurma.java
@@ -25,7 +25,7 @@ public class cadastroTurma {
 	/**
 	 * Launch the application.
 	 */
-	public static void cadastroTurma() {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -79,13 +79,13 @@ public class cadastroTurma {
 		frame.getContentPane().add(lblOferecerCurso);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(576, 380, 168, 39);
+		textField_1.setBounds(619, 380, 168, 39);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Oferecer Disciplina Nova");
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 13));
-		lblNewLabel.setBounds(576, 355, 168, 14);
+		lblNewLabel.setBounds(619, 355, 168, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
@@ -102,9 +102,9 @@ public class cadastroTurma {
 		tableCurso = new JTable();
 		tableCurso.setBounds(63, 138, 183, 192);
 		frame.getContentPane().add(tableCurso);
-		
+		//http://stackoverflow.com/questions/10620448/most-simple-code-to-populate-jtable-from-resultset
 		tableDisciplina = new JTable();
 		tableDisciplina.setBounds(576, 138, 183, 191);
 		frame.getContentPane().add(tableDisciplina);
-	}
+	} 
 }
